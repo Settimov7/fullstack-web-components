f4c2c004c5b8f78e75e5fd2c38f7b13d2c272e4c
Settimo <prytkov.nikita.set@gmail.com>
2022-11-09 14:08:35 +0400

    feat: button component

--- packages/component/src/button/button.stories.js
+++ packages/component/src/button/button.stories.js
@@ -18,3 +18,35 @@ Primary.args = {
   label: 'Button',
   variant: 'primary',
 };
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+  variant: 'secondary',
+};
+
+let icon = null;
+
+if (window.FontAwesome) {
+  icon = window.FontAwesome.icon({ prefix: "fas", iconName: 'plus'});
+}
+
+const svg = icon?.node[0];
+
+export const Icon = Template.bind({});
+Icon.args = {
+  variant: "icon icon-close",
+  label: svg,
+}
+
+function DisabledTemplate({ label, variant }) {
+  return html`
+    <button class='${variant}' is='in-button' disabled>${label}</button>
+  `;
+}
+
+export const Disabled = DisabledTemplate.bind({});
+Disabled.args = {
+  variant: "primary",
+  label: 'Submit',
+}
--- packages/component/src/button/button.ts
+++ packages/component/src/button/button.ts
@@ -1,10 +1,120 @@
+const buttonStyles = `
+  .in-button.primary {
+    box-sizing: border-box;
+    min-width: 180px;
+    min-height: 44px;
+    padding: 0px;
+    border: 2px solid var(--color-blue-500);
+    border-radius: 12px;
+    
+    font-size: var(--font-body-md);
+    font-weight: var(--font-weight-button);
+    color: var(--color-white);
+    
+    background-color: var(--color-blue-500);
+    cursor: pointer;
+  }
+  
+  .in-button.secondary {
+    box-sizing: border-box;
+    min-width: 180px;
+    min-height: 44px;
+    padding: 0px;
+    border: 2px solid var(--color-blue-500);
+    border-radius: 12px;
+    
+    font-size: var(--font-body-md);
+    font-weight: var(--font-weight-button);
+    color: var(--color-blue-500);
+    
+    background-color: var(--color-white);
+    cursor: pointer;
+  }
+  
+  .in-button.icon {
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 44px;
+    height: 44px;
+    padding: var(--padding-xs);
+    border: 2px solid var(--color-blue-500);
+    border-radius: 12px;
+    
+    font-size: var(--font-body-icon);
+    font-weight: var(--font-weight-button);
+    color: var(--color-blue-500);
+    
+    background-color: var(--color-white);
+    cursor: pointer;
+  }
+  
+  .icon svg {
+    width: 100%;
+    height: 100%;
+  }
+  
+  .icon.icon-close svg {
+    transform: rotateZ(45deg);
+  }
+  
+  .in-button:focus {
+    border: 2px solid var(--color-black);
+    outline: none;
+  
+    color: var(--color-black);
+  
+    background-color: var(--color-white);
+  }
+  
+  .in-button:active {
+    border: 2px solid var(--color-neutral-500);
+    outline: none;
+  
+    color: var(--color-neutral-500);
+    
+    background-color: var(--color-white);
+  }
+  
+  .in-button[disabled] {
+    opacity: var(---color-disable);
+    background: var(--color-disable);
+    border: var(--border-disable);
+    color: var(--color-neutral-500);
+    cursor: default;
+  }
+  
+  .in-button[disabled]:focus,
+  .in-button[disabled]:active {
+    border: var(--border-disable);
+    outline: none;
+    box-shadow: none;
+  }
+`;
+
 export class ButtonComponent extends HTMLButtonElement {
   constructor() {
     super();
   }
 
+  addStylesheet() {
+    const head = document.head;
+
+    if (document.getElementById('in-button-style')) {
+      return;
+    }
+
+    const style = document.createElement('style');
+
+    style.setAttribute('id', `in-button-style`);
+    style.textContent = buttonStyles;
+    head.appendChild(style);
+  }
+
   connectedCallback() {
     this.classList.add('in-button');
+    this.addStylesheet();
   }
 }
 
